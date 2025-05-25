5d3985cc91952dac25b7c0a49888bc7a6a1d6ba8
tm01292005 <aaa@test.com>
2025-05-25 12:14:55 +0900

    例外処理の実装

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,10 +31,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100; // セントに変換
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error("Error creating invoice:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -51,11 +55,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error("Error updating invoice:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -63,6 +71,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // 請求書削除
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
