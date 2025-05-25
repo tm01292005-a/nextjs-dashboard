507af42a7d261c5b25cdcd1d090eac68838b75dd
tm01292005 <aaa@test.com>
2025-05-25 13:29:56 +0900

    メタデータ追加

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,6 +8,12 @@ import { Suspense } from "react";
 import { queryObjects } from "v8";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
